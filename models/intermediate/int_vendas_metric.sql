15db5879bdf14c44328ab3cc0ee00106476e5064
Barbara Gontijo <barbara.gontijo@unimedbh.com.br>
2024-12-18 15:12:03 +0000

    Construindo fato vendas

--- models/intermediate/int_vendas_metric.sql
+++ models/intermediate/int_vendas_metric.sql
@@ -38,6 +38,42 @@ with
 , metricas as (
     select
         *
-        ,
+        , QTD_VENDA * VALOR_UNITARIO_VENDA as VALOR_BRUTO
+        , (QTD_VENDA * (VALOR_UNITARIO_VENDA- VALOR_DESCONTO_UNIT_VENDA)) as VALOR_LIQ
+        , FRETE_VENDA/ (COUNT(*) OVER(PARTITION BY PK_VENDA)) AS FRETE_RATEADO
+        , CASE
+            WHEN VALOR_DESCONTO_UNIT_VENDA>0 THEN TRUE
+            ELSE FALSE
+        END AS DESCONTO
     from joined
 )
+
+    , FINAL_SELECT AS (
+        SELECT 
+            PK_VENDA_DETALHE
+            , PK_VENDA
+            , FK_PRODUTO
+            , FK_CUSTOMER
+            , FK_CARTAO
+            , FK_CONVERSAO 
+            , DT_VENDA
+            , DT_ENTREGA_PREVISTA
+            , DT_ENVIO
+            , SUBTOTAL_VENDA
+            , TAXA_VENDA
+            , QTD_VENDA
+            , VALOR_TOTAL_VENDA
+            , VALOR_UNITARIO_VENDA
+            , VALOR_DESCONTO_UNIT_VENDA
+            , VALOR_BRUTO
+            , VALOR_LIQ
+            , CAST (FRETE_RATEADO AS NUMERIC (38,4)) AS FRETE_RATEADO
+            , DESCONTO
+            , STATUS_VENDA
+            , COD_PROMO_VENDA
+
+        FROM metricas
+    )
+
+SELECT *
+FROM FINAL_SELECT
